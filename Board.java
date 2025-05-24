e48685ae4ee0bfcbd1c0dca6074f158d706f1a30
unknown <davijannsen@gmail.com>
2025-05-24 10:05:26 -0300

    melhoria no tabuleiro visual + limites para o tabuleiro + esboço da movimentação do robô

--- Board.java
+++ Board.java
@@ -10,49 +10,68 @@ public class Board
     // private Robot[] robots;
     private int foodX;
     private int foodY;
-    private List<Robot>[][] tabuleiroVisual;
+    public List<Robot>[][] tabuleiroVisual;
 
     public Board(int foodX, int foodY) 
     {
         this.foodX = foodX;
         this.foodY = foodY;
-
-        tabuleiroVisual = new ArrayList[4][4];
-
+        this.tabuleiroVisual = new ArrayList[4][4];
         for (int i = 0; i < 4; i++) 
         {
             for (int j = 0; j < 4; j++) {
-                tabuleiroVisual[i][j] = new ArrayList<>();
+                this.tabuleiroVisual[i][j] = new ArrayList<>();
             }
         }
-        Robot robot = new Robot("vermelho");
-        tabuleiroVisual[0][0].add(robot);
-        
+            Robot robot = new Robot("vermelho");
+            tabuleiroVisual[0][0].add(robot);
     }
+
     public void printVisualBoard() {
         System.out.println("\n==========================");
-        for (int i = 0; i < 4; i++) {  
-            for (int j = 0; j < 4; j++) {  
-                if (i == foodY && j == foodX) {
-                    System.out.print(".[Comida]\t");
-                } else if (!tabuleiroVisual[i][j].isEmpty()) {
-                    System.out.print(".[");
-                    for (Robot r : tabuleiroVisual[i][j]) {
-                        System.out.print(r.getColor() + "");
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    if ((i == foodY)&& (j == foodX)){
+                        System.out.print(".[" + 5 + "]\t"); 
                     }
-                    System.out.print("]\t");
-                } else {
-                    System.out.print(".[ ]\t");
+                    else if (!tabuleiroVisual[i][j].isEmpty()) {    
+                        System.out.print(".[");
+                        for (Robot r : tabuleiroVisual[i][j]) {
+                            System.out.print(r.getColor() + ""); 
+                        }
+                        System.out.print("]\t");
+                    } else {
+                    System.out.print(".[ VAZIO ]\t");
+                } 
                 }
+                System.out.println();
             }
-            System.out.println();  
-        }
+        
         System.out.println("=================================");
     }
-    
+
+    public void moveRobotWithLimits(Robot r) {
+        int newX = r.position[0];
+        int newY = r.position[1];
+
+    if (newX < 0) {
+        newX = 0;
+        System.out.println("Você esbarrou na parede esquerda!");
+    } else if (newX > 3) {
+        newX = 3;
+        System.out.println("Você esbarrou na parede direita!");
+    }
+
+    if (newY < 0) {
+        newY = 0;
+        System.out.println("Você esbarrou na parede superior!");
+    } else if (newY > 3) {
+        newY = 3;
+        System.out.println("Você esbarrou na parede inferior!");
+    }
+    }
     public void updateBoard(List<Robot>[][]  tabuleiroVisual){
 
     }
     
 }
-
--- Robot.java
+++ Robot.java
@@ -1,37 +1,43 @@
+import java.util.Scanner;
 public class Robot {
-    protected int positionX;
-    protected int positionY;
+    protected int[] position;
     protected String color;
+    Scanner sc = new Scanner(System.in);
 
     public Robot(String color) {
-        this.positionX = 0;
-        this.positionY = 0;
+        position = new int[2];
         this.color = color;
     }
 
-    public void moveRobot(int positionX, int positionY){
-        this.positionX = positionX;
-        this.positionY = positionY;
-    }
+    public void moveRobot(String sc){
+        int positionX = position[0];
+        int positionY = position[1];
+        sc = sc.toLowerCase();
 
-    public int getPositionX() {
-        return positionX;
-    }
-
-    public int getPositionY() {
-        return positionY;
-    }
+        switch(sc)
+        {
+            case "up":
+                positionY--;
+                break;
+            case "down":
+                positionY++;
+                break;
+            case "left":
+                positionX--;
+                break;
+            case "right":
+                positionX++;
+                break;
+            default:
+                System.out.println("Comando inválido!");
+                return; 
+        }
+        this.position[0] = positionX;
+        this.position[1] = positionY;
+        }
 
     public String getColor() {
         return color;
-    }
-
-    public void setPositionX(int positionX) {
-        this.positionX = positionX;
-    }
-
-    public void setPositionY(int positionX) {
-        this.positionY = positionX;
     }
 
     public void setcolor(String color) {
