aed3d2331e1dd41e8e9ef920ba7ec305e459bae1
Apolo <apolovictorj.c@gmail.com>
2025-05-25 16:30:07 -0300

    Ajuste na main

--- Main.java
+++ Main.java
@@ -4,12 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Board board = new Board(2, 3);
-        Robot robot = board.tabuleiroVisual[0][0].get(0);  // pegar o robô que já está no tabuleiro
+        Robot robot = null;
 
-        while (true) {
+        int menu = 1;
+        while (menu == 1) {
+            System.out.println("Escreva uma cor para o robô e comece a jogar!");
+            String comando = scanner.nextLine();
+            robot = new Robot(comando);
+            menu = 2;
+        }
+        while (menu == 2) {
+            board.updateBoard(robot);
             board.printVisualBoard();
 
-            System.out.print("Digite o comando para mover o robô (up, down, left, right) ou 'sair' para encerrar: ");
+            System.out.print("Digite o comando para mover o robô (1/up, 2/down, 3/left, 4/right) ou 'sair' para encerrar: ");
             String comando = scanner.nextLine();
 
             if (comando.equalsIgnoreCase("sair")) {
@@ -17,14 +25,15 @@ public class Main {
                 break;
             }
 
-            // Move o robô conforme comando
-            robot.moveRobot(comando);
+            if (comando.matches("[1-4]")) {
+                int movimento = Integer.parseInt(comando);
+                robot.moveRobot(movimento); 
+            } 
+            else {
+                robot.moveRobot(comando); 
+            }
 
-            // Corrige posição caso ultrapasse limites
             board.moveRobotWithLimits(robot);
-
-            // Atualiza o tabuleiro visual para refletir a nova posição
-            board.updateBoard(robot);
 
         }
         scanner.close();
