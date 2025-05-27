59dd62ad508dd1b04aa682bb2f71891cd22aec20
Guilherme Gaspar <guilhermevgaspar07@gmail.com>
2025-05-27 17:51:53 -0300

    Mudando ID pra final

--- Obstacle.java
+++ Obstacle.java
@@ -1,6 +1,4 @@
 public abstract class Obstacle {
-
-    protected int id;
     protected int[] position;
     private static int nextId = 1;      
     private final int id; 
@@ -16,10 +14,6 @@ public abstract class Obstacle {
     public abstract void bater(Robot robot);
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
     public int[] getPosition() {
         return position;
